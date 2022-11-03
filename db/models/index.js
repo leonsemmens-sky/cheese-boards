364d2d95ff294649063c2edfabae58545dc5a7bd
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-11-03 16:50:14 +0000

    Update index.js

--- db/models/index.js
+++ db/models/index.js
@@ -1,11 +1,11 @@
-import User from "./User.model"
-import Board from "./Board.model"
-import Cheese from "./Cheese.model"
+import User from "./User.model";
+import Board from "./Board.model";
+import Cheese from "./Cheese.model";
 
-User.hasMany(Board)
-Board.belongsTo(User)
+User.hasMany(Board);
+Board.belongsTo(User);
 
-Board.belongsToMany(Cheese, { through: "Board_Cheese" })
-Cheese.belongsToMany(Board, { through: "Board_Cheese" })
+Board.belongsToMany(Cheese, { through: "Board_Cheese" });
+Cheese.belongsToMany(Board, { through: "Board_Cheese" });
 
-export default { User, Board, Cheese }
+export { User, Board, Cheese };
