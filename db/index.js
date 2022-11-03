a6b468c4aec0f9db24def3e8ad526d18943e3179
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-11-03 16:50:04 +0000

    redefine __dirname since ES6 does not have the global

--- db/index.js
+++ db/index.js
@@ -1,8 +1,11 @@
 import { Sequelize } from "sequelize";
 import path from "path";
 
+import * as url from "url";
+const _dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 export default new Sequelize({
-    dialect: "sqlite",
-    storage: path.join(__dirname, "delicious_cheeses.sqlite"),
-    logging: false,
+	dialect: "sqlite",
+	storage: path.join(_dirname, "delicious_cheeses.sqlite"),
+	logging: false,
 });
