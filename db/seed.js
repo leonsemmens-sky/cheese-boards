e5b424e96414be063fcac52e27df1d93df71c21c
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-11-03 16:50:22 +0000

    Update seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,7 +1,11 @@
-import CHEESES from "./cheeses.json";
-import Cheese from "./models/Cheese.model";
+import CHEESES from "./cheeses.json" assert { type: "json" };
+import { User, Board, Cheese } from "./models";
 
-export default async function seed() {
+async function seed() {
+	await User.sync({ force: true });
+	await Board.sync({ force: true });
+	await Cheese.sync({ force: true });
+
 	let data = [];
 
 	for (let cheese of Object.keys(CHEESES)) {
@@ -13,3 +17,5 @@ export default async function seed() {
 
 	await Cheese.bulkCreate(data);
 }
+
+seed();
